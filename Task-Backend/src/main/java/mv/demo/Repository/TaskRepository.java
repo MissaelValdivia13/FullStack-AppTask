a2916b7ba4f22bc79fed1820986290a2ffdc7511
Missael Valdivia <missael121303@gmail.com>
2024-10-10 20:25:46 -0600

    Eliminar elementos

--- Task-Backend/src/main/java/mv/demo/Repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package mv.demo.Repository;
-
-import mv.demo.Model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
